3f8917e35bb06521c434ba7e61e3c8f4a45e97c5
Nisha0690 <nisha.sen.me@gmail.com>
2021-07-05 16:47:17 +0530

    Second user will push

--- GitSecondUser/src/main/java/SimpleCode.java
+++ GitSecondUser/src/main/java/SimpleCode.java
@@ -4,6 +4,8 @@ public class SimpleCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hello");
+			System.out.println("Nisha");
+			System.out.println("Sen");
 	}
 
 }
